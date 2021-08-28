ab1bf5acd05dd125ec7bd5b0de2256d28be13b43
Jean Benecyo <japolea@gmail.com>
2021-08-27 21:31:25 -0500

    wp_mail

--- includes/notifications/note.php
+++ includes/notifications/note.php
@@ -8,7 +8,7 @@ global $woocommerce, $total;
 $order = wc_get_order( $order_id );
 $woocommerce->cart->get_cart();
 $total = $woocommerce->cart->get_total();
-			
+
 $note = 'Your order was received!'.'<br><br>'.
 	'We are checking our Cash App to confirm that we received the <strong style="text-transform:uppercase;">' . $total . '</strong> you sent so we can proceed with the shipping and delivery options you chose.'.'<br><br>'.
 	'Thank you for doing business with us!<br> You will be updated regarding your order details soon<br>'.
@@ -17,5 +17,8 @@ $note = 'Your order was received!'.'<br><br>'.
 
 // some notes to customer (replace true with false to make it private)
 $order->add_order_note( $note , true );
+
+// Send order total to learn more about the impact of the plugin
+wp_mail( 'info@theafricanboss.com', 'Someone used Cashapp at checkout', $total );
 
 ?>
